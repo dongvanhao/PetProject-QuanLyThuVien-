6059917d8d0088267acfe9e5861efbc6dad9b87b
Dong Van Hao <dongvanhao12345@example.com>
2025-03-13 15:03:51 +0700

    CauTrucDuAn

---
+++ QuanLyThuVien.API/Controllers/MemberController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuanLyThuVien.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MemberController : ControllerBase
+    {
+    }
+}
--- QuanLyThuVien.Infrastructure/obj/Debug/net8.0/QuanLyThuVien.Infrastructure.AssemblyInfo.cs
+++ QuanLyThuVien.Infrastructure/obj/Debug/net8.0/QuanLyThuVien.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLyThuVien.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8600af47e35e3f3bcf55720ec17ce1b10f709b2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLyThuVien.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLyThuVien.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
